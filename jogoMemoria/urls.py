a140d1fbcab15fe215f5b078d6721ff3afe07446
HigoPontes <ponteshigo@gmail.com>
2024-12-12 17:04:34 -0300

    Mudei o login e removir o metodo antigo para salvar as tentativas

--- jogoMemoria/urls.py
+++ jogoMemoria/urls.py
@@ -1,15 +1,16 @@
 # urls.py: usamos essa parte para criar as URL(link) entre as paginas 
-
+from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from jogoMemoria.views import index, TelaInicial,save #Lembrar de adicionar uma virgula
-# E importante colocar o nome da pagina a qual vc desse adicionar a url no import do jogoMemoria.views
+from jogoMemoria.views import index, TelaInicial,save, usuario_login #Lembrar de adicionar uma virgula
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    #Aqui estão as url e a sua sintaxe
-    #'nome do path',template que vai utilizar, name="Apelido que vai usar para chamar o path"
-    path('',index,name="index"),
-    path('TelaInicial/',TelaInicial,name="TelaInicial"),
-    path('save',save,name='save')
+    path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='registro/login.html'), name='login'),
+    path('', usuario_login, name="login"),
+    path('index/',index,name="index"),
+    path('telaInicial/',TelaInicial,name="TelaInicial"),
+    path('save',save,name='save'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- jogoMemoria/views.py
+++ jogoMemoria/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from jogoMemoria.models import Jogador
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 #def define uma função
 #sintaxe def nomedafunção(o oque ela vai fazer):
@@ -8,9 +10,25 @@ from jogoMemoria.models import Jogador
 def login(request):
     return render(request,'login.html')
 
+def usuario_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            return render(request, 'registration/login.html', {'error': 'Usuário ou senha inválidas'})
+
+    # jogo_listar = listar_jogos() # função para pegar a tabela e listar
+    return render(request, 'registro/login.html')#, {'jogo_listar': jogo_listar})
+
+@login_required
 def index(request):
     return render(request,'index.html')
 
+@login_required
 def TelaInicial(request):
     jogadores = Jogador.objects.all()
     jogadores_lista = []
