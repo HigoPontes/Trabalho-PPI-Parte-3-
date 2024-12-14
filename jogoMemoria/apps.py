e2fa79cbb0751899cd0333fb5dab22a35c0a3b81
HigoPontes <ponteshigo@gmail.com>
2024-12-14 16:09:11 -0300

    Tentei fazer os negocio funcionar e nada

--- jogoMemoria/apps.py
+++ jogoMemoria/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class JogomemoriaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'jogoMemoria'
+
+def ready(self):
+        import jogoMemoria.signals
--- jogoMemoria/models.py
+++ jogoMemoria/models.py
@@ -1,7 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 #Aqui eu estou definindo o modelo da lista do banco de dados. 
 #Neste caso o Jogador que vai ter o seu nome, o tempo e as jogadas
 class Jogador(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.nickname
+
+class Jogo(models.Model):
     nome=models.CharField(
         max_length=100,null=False
     )
@@ -11,8 +19,7 @@ class Jogador(models.Model):
     jogadas=models.CharField(
         max_length=20,null=False
     )
-
-#    def_str_(self):
-#       return f"Jogador [nome={self.nome}]"
+    def __str__(self):
+        return self.nome
 
 # Create your models here.
